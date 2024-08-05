4a57180c02f5a0442f8f60a7715336ecea933a27
Shubham <sk715152@gmail.com>
2024-08-05 16:06:39 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Header from "./component/Header";
 import AddItem from "./component/AddItem";
 import List from "./component/List";
